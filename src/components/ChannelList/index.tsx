f179f51d23cece05e645cb94da35b3a6aa2231e9
Luciano Victor <lucianocarvallho7@gmail.com>
2022-06-28 12:28:23 -0300

    feat: add section ChannelList and styles

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,20 @@
+import { Container, Category, AddCategoryIcon} from './styles';
+
+import ChannelButton from '../ChannelButton';
+
+const ChannelList: React.FC = () => {
+  return (
+    <Container >
+      <Category>
+        <span>Canais de texto</span>
+      </Category>
+      <ChannelButton channelName="chat-livre" />
+      <ChannelButton channelName="trabalho" />
+      <ChannelButton channelName="lolzinho" />
+      <ChannelButton channelName="csgo" />
+      <ChannelButton channelName="valorant" />
+    </Container>
+  );
+};
+
+export default ChannelList;
